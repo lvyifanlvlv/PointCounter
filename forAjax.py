d209ca7447b730eeff79939b46e736f95d074434
lvyifanlvlv <you@example.com>
2018-12-25 20:37:59 +0800

    lyf

--- forAjax.py
+++ forAjax.py
@@ -1,19 +1,27 @@
 import json
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
 
-@app.route("/html")
+@app.route("/")
 def html():
     return render_template('ajaxtest.html')
 
 @app.route("/111",methods=['POST'])
 def ajax_demo():
-    print("aaaa")
-    print(request.form['a'])
-    print(request.form['b'])
-    print(request.form['c'])
+    print(request.form['usrname'])
+    print(request.form['groupname'])
+    print(request.form['projectname'])
+    id=0
+    TIME=os.time
+    NAME=request.form['usrname']
+    RGROUP=request.form['groupname']
+    RNAME=request.form['projectname']
+    PCOUNT=1
+    ISFINISHED=0
+
     # if request.method == 'POST':
     #     ret = {'status':False,'message':''}
     #     print("hahahaha")
